376994c630e08f90cb6d5e9cd16bca6eb84d0f4f
Zulfiqar-Ali10 <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2024-03-02 00:16:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// Chapter no 4;
+
+// Question no 1
+// var a = 1, b = 2, c = 3
+
+// Questio no 2
+// Legal Variable Names
+// 1: firstName
+// 2: myVar
+// 3: _variable
+// 4: $price
+// 5: camelCaseVariable
+
+// Illegal Variable Names
+
+// 1: 123variable
+// 2: my-variable
+// 3: let
+// 4: variable name
+// 5: !illegal
+
+// Chapter no 6;
+// var a = 10;
+// a = ++a
+// document.write(a);
+
+// var a = 10;
+// a = a++
+// document.write(a);
+
+// var a = 10;
+// a = --a
+// document.write(a);
+
+// var a = 10;
+// a = a--
+// document.write(a);
+
+// Question no 2
+// var a = 2, b = 1;
+// var result = a-- - --b + ++b +b--;
+
+// document.write("result is 3");
+
+// Question no 3
+
+
+
+
